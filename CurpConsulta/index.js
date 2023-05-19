5dce2e258e5830ab8dd1b0d1ac8f0ca94e4a3545
Omar Barrios <omar@fibotech.mx>
2023-05-18 19:20:21 -0700

    azure function V3
    azure function V3 funcional

---
+++ CurpConsulta/index.js
@@ -0,0 +1,95 @@
+const {v1: uuidv1 } = require('uuid'),
+moment = require("moment"),
+DB = require("../Shared/Utils/DB"),
+scrapper = require("../Shared/Service/scrapper");
+
+
+module.exports = async function (context, req) {
+
+    let body = null;
+    const strBody = JSON.stringify(req.body);
+    const reqJson = req.body;
+    const uuid = uuidv1();
+    const request_time = moment().utc();
+    let idLog = "";
+    let response = { status: "error", message: "", data: null, code: 0 };
+
+    try{
+        
+        idLog = await SaveRequest(strBody, uuid, request_time);
+
+        response = await errorRequest(reqJson, response);
+
+        if(response.code !== 0) body = response;
+
+        if(response.code === 0 && reqJson.tipo_busqueda == "curp") body = await scrapper.ObtenerCurp(reqJson.curp, response);
+
+        if(response.code === 0 && reqJson.tipo_busqueda == "datos") body = await scrapper.ObtenerCurpByData(reqJson.clave_entidad, reqJson.dia_nacimiento, reqJson.mes_nacimiento,
+            reqJson.nombres, reqJson.primer_apellido, reqJson.segundo_apellido, reqJson.anio_nacimiento, reqJson.sexo, response);
+
+    }
+    catch(err){
+        response.code = 500;
+        response.message = "Error al procesar la petición";
+        response.data = null;
+        response.status = "error";
+
+        body = response;
+    }
+    finally{
+        if(idLog !== "") await SaveResponse(idLog, response, response.status, request_time)
+    }    
+    
+    context.res = {
+        // status: 200, /* Defaults to 200 */        
+        headers: {
+            "content-type": "application/json"
+        },
+        body: body
+    };
+}
+
+
+function SaveRequest(body, guid, reqTime){
+
+    return DB.insert({
+        request: body, 
+        guid: guid, 
+        request_datetime: reqTime.format("YYYY-MM-DD HH:mm:ss")
+    }, DB.tabla.consultarCurplog)    
+    .then(id => { return ( id);})
+    .catch(err => { return console.log(err, req.guid)});
+}
+
+function SaveResponse(idLog, response, status, reqTime){
+
+    let response_time = moment().utc();
+    let duration = moment.duration(response_time.diff(reqTime)).asSeconds();
+
+    return DB.update({
+        response: JSON.stringify(response),
+        response_datetime: response_time.format("YYYY-MM-DD HH:mm:ss"),                        
+        status: status,
+        tiempo: duration
+    }, DB.tabla.consultarCurplog, `id=${idLog}`)
+}
+
+async function errorRequest(req, response){
+
+    if(!req) response.message = "Error en request";
+    else if(!req.tipo_busqueda)  response.message = "Parámetro tipo_busqueda requerido";
+    else if(req.tipo_busqueda != "curp" && req.tipo_busqueda != "datos") response.message = "Parámetro tipo_busqueda valor invalido";
+    else if(req.tipo_busqueda == "curp" && !req.curp) response.message = "Parámetro curp requerido";
+    else if(req.tipo_busqueda == "datos" && !req.clave_entidad) response.message = "Parámetro clave_entidad requerido";
+    else if(req.tipo_busqueda == "datos" && !req.dia_nacimiento) response.message = "Parámetro dia_nacimiento requerido";
+    else if(req.tipo_busqueda == "datos" && !req.mes_nacimiento) response.message = "Parámetro mes_nacimiento requerido";
+    else if(req.tipo_busqueda == "datos" && !req.nombres) response.message = "Parámetro nombres requerido";
+    else if(req.tipo_busqueda == "datos" && !req.primer_apellido) response.message = "Parámetro primer_apellido requerido";
+    else if(req.tipo_busqueda == "datos" && !req.segundo_apellido) response.message = "Parámetro segundo_apellido requerido";
+    else if(req.tipo_busqueda == "datos" && !req.anio_nacimiento) response.message = "Parámetro anio_nacimiento requerido";
+    else if(req.tipo_busqueda == "datos" && !req.sexo) response.message = "Parámetro sexo requerido";
+
+    if(response.message !== "") response.code = 120;
+
+    return response;
+}
---
+++ Shared/Service/scrapper.js
@@ -0,0 +1,478 @@
+// const puppeteer = require('puppeteer'),
+const puppeteer = require('puppeteer-extra'),
+        StealthPlugin = require("puppeteer-extra-plugin-stealth"),
+        cheerio = require('cheerio'),
+        UserAgent = require("user-agents"),
+        ac = require("@antiadmin/anticaptchaofficial"),
+        
+        PROXY_USERNAME = process.env.PROXY_USERNAME,
+        PROXY_PASSWORD = process.env.PROXY_PASSWORD,
+        PROXY_SERVER = process.env.PROXY_SERVER,
+        PROXY_SERVER_PORT = process.env.PROXY_SERVER_PORT,
+        PROXY_ACTIVE = JSON.parse(process.env.PROXY_ACTIVE),
+
+
+        urlConsulta = process.env.CURP_URL_CONSULTA,
+        urlDescarga = process.env.CURP_URL_DESCARGA,
+
+        antiCaptchaKey = process.env.ANTICAPTCHA_KEY,
+        gKEY = process.env.G_KEY,
+        urlTarget = process.env.CURP_URL;
+
+const myConst = {
+    AN: "Alta nominal",
+    AH: "Alta con homonimia",
+    CRA: "Curp reactivada",
+    RCN: "Registro de cambio no afectando a curp",
+    RCC: "Registro de cambio afectando acurp",
+    BD: "Baja por defuncion",
+    BDA: "Baja por duplicidad",
+    BCC: "Baja por cambio en curp",
+    BCN: "Baja no afectando a curp"
+    };
+          
+let finalResponse = null;
+let downloadResponse = null;
+
+
+
+module.exports = {
+    ObtenerCurp: async function(curp, response)
+    {
+        return await ScrappCurpbyCurp(curp, response);
+    },
+    ObtenerCurpByData: async function(clave_entidad, dia_nacimiento, mes_nacimiento, nombres, primer_apellido, segundo_apellido, anio_nacimiento, sexo, response){
+        return await ScrappCurpbyData(clave_entidad, dia_nacimiento, mes_nacimiento, nombres, primer_apellido, segundo_apellido, anio_nacimiento, sexo, response);
+    }
+}
+
+async function ScrappCurpbyCurp(curp, response){
+
+    let responseFinal = response;
+    let errorInfo = "";
+    let file = "";
+    let errData = "ini";
+
+    try{
+        for(let i = 0; i < 3; i++){
+
+            puppeteer.use(StealthPlugin());
+
+            errData += "\nUse StealthPlugin";
+
+            let browser = await StartBrowser();
+
+            errData += "\nUse startBrowser";
+
+            const userAgent = new UserAgent({deviceCategory: "desktop"});
+            
+            errData += "\nUse useragent";
+            try{
+
+                let page = await browser.newPage();
+
+                errData += "\nUse newPage";
+
+                await page.setUserAgent(userAgent.random().toString());
+
+                errData += "\nUse setUserAgent";
+
+                if(PROXY_ACTIVE){
+                    await page.authenticate({
+                        username: PROXY_USERNAME,
+                        password: PROXY_PASSWORD,
+                    });
+                }
+            
+                await page.goto(urlTarget, {timeout: 60000});
+                let bodyHTML = await page.evaluate(() => document.body.innerHTML);
+                let $ = cheerio.load(bodyHTML);
+
+                errData += "\nUse gotoPage";
+
+                
+
+                await page.waitForSelector('#ember313 > div > div > iframe', 3000);
+                const elementHandle = await page.$('.g-recaptcha > div > div > iframe')
+                const frame = await elementHandle.contentFrame()                
+
+                await wait(2 * 1000);
+
+                let responseData = "";
+
+                ac.setAPIKey(antiCaptchaKey);
+
+                await ac.solveRecaptchaV2Proxyless(urlTarget, gKEY)
+                    .then(gresponse => {
+                    console.log('g-response: '+gresponse);
+                    responseData = gresponse;
+                })
+                .catch(error => console.log('test received error '+error));
+
+                if(responseData === "") continue;
+
+                
+                await page.evaluate(val => document.querySelector('#curpinput').value = val, curp);            
+                await page.evaluate(val => document.querySelector('#g-recaptcha-response').value = val, responseData);
+
+                await page.setRequestInterception(true);                
+                page.on('request', handleRequest);
+                page.on('response', handleResponse);
+
+                const btnSearch = await page.$('#searchButton');
+                await btnSearch.evaluate( btnSearch => btnSearch.click() );
+
+                await wait(1 * 1000);
+                
+
+                // finalResponse = await page.waitForResponse(response =>
+                //     response.url() === urlConsulta && response.status() === 200
+                // );
+
+                jsonResponse = await finalResponse.json();
+
+                jsonResponseFinal = formatJsonResponse(jsonResponse, responseData);
+                
+
+                if(typeof jsonResponse == "object" &&  jsonResponse.codigo === "01" )
+                {
+                    jsonResponseFinal["files"] = [];
+                    jsonResponseFinal.files[0] = await GetFile(page);
+
+                    responseFinal.data = jsonResponseFinal;
+                    responseFinal.status = "success";
+                    responseFinal.code = 200;
+                    responseFinal.message = "resolve!";
+
+                    return responseFinal;
+                }
+                else{
+                    responseFinal.data =  jsonResponseFinal;
+                    responseFinal.message =  "error durante procesamiento";
+                    responseFinal.code =  120;
+                    responseFinal.status = "error";
+
+                    return responseFinal;
+                }
+            }
+            catch(err){
+                console.log(err);
+
+                responseFinal.data =  null;
+                responseFinal.message =  "error durante procesamiento";
+                responseFinal.code =  140;
+                responseFinal.status = "error";
+            }
+            finally{
+                await browser.close();    
+            }
+        }
+    }
+    catch(err){
+
+        responseFinal.data = {error : JSON.stringify(errData) };
+        responseFinal.message =  "error durante procesamiento";
+        responseFinal.code =  141;
+        responseFinal.status = "error";        
+    }
+
+    return responseFinal;
+    
+}
+
+async function ScrappCurpbyData(clave_entidad, dia_nacimiento, mes_nacimiento, nombres, primer_apellido, segundo_apellido, anio_nacimiento, sexo, response){
+
+    try{
+
+        let responseFinal = response;
+        let errorInfo = "";
+        let file = "";
+
+
+        for(let i = 0; i < 3; i++){
+
+            puppeteer.use(StealthPlugin());
+
+            let browser = await StartBrowser();
+
+            const userAgent = new UserAgent({deviceCategory: "desktop"});
+            
+            await page.setUserAgent(userAgent.random().toString());
+
+            try{
+
+
+                let page = await browser.newPage();
+
+                if(PROXY_ACTIVE){
+                    await page.authenticate({
+                        username: PROXY_USERNAME,
+                        password: PROXY_PASSWORD,
+                    });
+                }
+            
+                await page.goto(urlTarget, {timeout: 180000});
+                let bodyHTML = await page.evaluate(() => document.body.innerHTML);
+                let $ = cheerio.load(bodyHTML);
+
+                await page.waitForSelector('#ember313 > div > div > iframe', 3000);
+                const elementHandle = await page.$('.g-recaptcha > div > div > iframe')
+                const frame = await elementHandle.contentFrame()                
+
+                await wait(2 * 1000);
+
+                let responseData = "";
+
+                ac.setAPIKey(antiCaptchaKey);
+
+                await ac.solveRecaptchaV2Proxyless(urlTarget, gKEY)
+                    .then(gresponse => {
+                    console.log('g-response: '+gresponse);
+                    responseData = gresponse;
+                })
+                .catch(error => console.log('test received error '+error));
+
+                if(responseData === "") continue;
+
+                await page.evaluate(val => document.querySelector('#g-recaptcha-response').value = val, responseData);
+
+                
+                await page.click('#datos a');
+
+                
+                await page.evaluate(val => document.querySelector('#nombre').value = val, nombres);            
+                await page.evaluate(val => document.querySelector('#primerApellido').value = val, primer_apellido);
+                await page.evaluate(val => document.querySelector('#segundoApellido').value = val, segundo_apellido);
+
+                await page.select('#diaNacimiento', dia_nacimiento);
+                await page.select('#mesNacimiento', mes_nacimiento);
+
+                await page.evaluate(val => document.querySelector('#selectedYear').value = val, anio_nacimiento);
+
+                await page.select('#sexo', sexo);
+                await page.select('#claveEntidad', clave_entidad);
+
+                await page.setRequestInterception(true);
+                
+                page.on('request', handleRequest);
+                page.on('response', handleRequest);
+
+                const btnSearch = await page.$('#searchButton');
+                await btnSearch.evaluate( btnSearch => btnSearch.click() );
+
+                await wait(1 * 1000);
+
+                // finalResponse = await page.waitForResponse(response =>
+                //     response.url() === urlConsulta && response.status() === 200
+                // );
+
+                jsonResponse = await finalResponse.json();
+
+                jsonResponseFinal = formatJsonResponse(jsonResponse, responseData);
+
+                if(typeof jsonResponse == "object" &&  jsonResponse.codigo === "01" )
+                {
+                    jsonResponseFinal["files"] = [];
+                    jsonResponseFinal.files[0] = await GetFile(page);
+
+                    responseFinal.data = jsonResponseFinal;
+                    responseFinal.status = "success";
+                    responseFinal.code = 200;
+                    responseFinal.message = "resolve!";
+
+                    return responseFinal;
+                }
+                else{
+                    responseFinal.data =  jsonResponseFinal;
+                    responseFinal.message =  "error durante procesamiento";
+                    responseFinal.code =  120;
+                    responseFinal.status = "error";
+
+                    return responseFinal;
+                }         
+            }
+            catch(err){
+                console.log(err);
+
+                responseFinal.data =  null;
+                responseFinal.message =  "error durante procesamiento";
+                responseFinal.code =  140;
+                responseFinal.status = "error";
+            }
+            finally{
+                await browser.close();    
+            }
+        }
+    }
+    catch(err){
+
+        responseFinal.data =  {error : JSON.stringify(err) };
+        responseFinal.message =  "error durante procesamiento";
+        responseFinal.code =  141;
+        responseFinal.status = "error";        
+    }
+
+    return responseFinal;
+}
+
+async function wait (ms) {
+    return new Promise((resolve, reject) => {
+      setTimeout(resolve, ms)
+    });
+}
+
+function formatData(tableData){
+    //convert table data to json
+    let data = [];
+
+    for(let i = 0; i < tableData.length; i++){
+        let row = {};
+        row[tableData[i][0]] = tableData[i][1];
+        data.push(row);
+    }
+}
+
+function formatJsonResponse(json, gdata){
+
+    let data = {};
+    data["gdata"] = gdata;  
+    data["curpdata"] = {};
+    
+
+    if(json === "")  data.curpdata["mensaje"] = "respuesta CURP no obtenida";
+    if(!json.hasOwnProperty("codigo")) data.curpdata = GetJsonError();
+    else if(json.codigo === "01" ) data.curpdata = GetJsonSuccess(json);
+    else if(json.codigo === "07" ) data.curpdata = GetJsonCatpchaInvalido(json);
+    else  data.curpdata = GetJsonErrorRespuestaCurp(json);    
+
+    return data;
+}
+
+function getValueStatus(valueStatusCurp){
+    return myConst[valueStatusCurp] || '';
+
+}
+
+function GetJsonError(){    
+    let json = {};    
+    
+    json["codigo"] = 120;
+    json["mensaje"] = "Error durante consulta";    
+    
+    return json;
+}
+
+function GetJsonErrorRespuestaCurp(json){
+    try{        
+        json["codigo"] = json.codigo;
+        json["mensaje"] = json.mensaje;
+    }
+    catch(err){
+        json["codigo"] = 120;
+        json["mensaje"] = "Error durante consulta";
+    }
+    
+    return json;
+}
+
+function GetJsonSuccess(json){ 
+
+    let data = [];
+    data = json.registros;    
+
+    data.forEach(element => {
+        element["descriptionStatusCurp"] = getValueStatus(element.statusCurp);
+    });
+    return data;
+}
+
+function GetJsonCatpchaInvalido(json){ 
+
+    let jsonData = {};
+    jsonData["codigo"] = json.codigo;
+    jsonData["mensaje"] = json.mensaje;        
+
+    return jsonData;
+}
+
+async function  StartBrowser(){
+    return (PROXY_ACTIVE)? await StartBrowserProxy() : await StartBrowserNoProxy();
+}
+
+async function  StartBrowserNoProxy(){
+    return await puppeteer.launch({
+        headless: 'new',
+        ignoreHTTPSErrors: true,
+        args: [
+            //`--proxy-server=http://${PROXY_SERVER}:${PROXY_SERVER_PORT}`, '--disable-web-security', '--disable-features=IsolateOrigins,site-per-process',
+            '--disable-web-security', '--disable-features=IsolateOrigins,site-per-process', '--no-sandbox'
+        ]
+    });
+}
+
+async function StartBrowserProxy(){
+    return await puppeteer.launch({
+        headless: 'new',
+        ignoreHTTPSErrors: true,
+        args: [
+            `--proxy-server=http://${PROXY_SERVER}:${PROXY_SERVER_PORT}`, '--disable-web-security', '--disable-features=IsolateOrigins,site-per-process',
+            '--disable-web-security', '--disable-features=IsolateOrigins,site-per-process,', '--no-sandbox'
+        ]
+    });
+}
+
+async function handleRequest(request) {
+    try {
+        // console.log(request);
+      await request.continue(); 
+    } catch (err) {
+      console.log(err);
+    }
+}
+
+async function handleResponse(response) {
+    try {
+        console.log(response.url());
+        if(response.url() === urlConsulta && response.status() === 200)
+            finalResponse = response;
+
+        if(response.url().includes(urlDescarga)  && response.status() === 200)
+            downloadResponse = response;
+        
+    } catch (err) {
+      console.log(err);
+    }
+}
+
+async function GetFile(page){
+
+    try{
+
+        await wait(2 * 1000);
+
+        
+        const btnDownload = await page.$('#download');
+        await btnDownload.evaluate( btnDownload => btnDownload.click() );
+
+        await wait(2 * 1000);
+
+        // let downloadResponse = await page.waitForResponse(response =>
+        //     response.url().includes(urlDescarga)  && response.status() === 200
+        // );
+
+
+        let pdfCurp = "";
+        
+        if(downloadResponse !== null) {
+            pdfCurp = await downloadResponse.text();//.replace("", "\n");
+            pdfCurp = pdfCurp.replace(/\\n/g, "");
+        }
+
+        return pdfCurp;
+    }   
+    catch(err)
+    {
+        console.log(err);
+        return "";
+    }
+}
